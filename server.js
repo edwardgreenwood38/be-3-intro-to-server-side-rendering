b940f38e8f9db307052629057e88f1f6c5c72f9d
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-21 11:31:49 -0500

    index bread controller added

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const app = express()
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
-})
+});
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js');
+app.use('/breads', breadsController);
+
 
 // LISTEN
 app.listen(PORT, () => {
