10678d1220c9945397a9201a1ab4cd50c3a43d27
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-21 11:34:31 -0500

    bread model added

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const breads = express.Router();
+const Bread = require('../models/bread.js');
+
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.send('This is the index at /breads');
+    res.send(Bread);
 });
 
 
