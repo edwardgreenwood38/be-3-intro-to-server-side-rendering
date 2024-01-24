fa3e66f5f2a6f6752e1248fc544ed6ee5084821f
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-23 20:52:33 -0500

    navigation and validation added

--- views/new.jsx
+++ views/new.jsx
@@ -6,9 +6,14 @@ function New () {
     return (
       <Default>
         <h2>Add a new bread</h2>
+
+        <div className="backButton">
+            <a href="/breads"><button>Go back to the index</button></a>
+        </div>
+
         <form action="/breads" method="POST">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input type="text" name="name" id="name" required />
 
           <label htmlFor="image">Image</label>
           <input type="text" name="image" id="image" />
