87472a4fc219464296adf2096676b88b9b24c603
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-25 20:39:52 -0500

    new edit page and route added

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,6 +19,15 @@ breads.get('/new', (req, res) => {
 })
 
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -47,6 +56,19 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 });
+
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 
 
 // DELETE
