35a4a4766a3f54569f3188895537ebba5969194b
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-25 20:45:23 -0500

    added edit button

--- views/show.jsx
+++ views/show.jsx
@@ -17,12 +17,14 @@ function Show ({bread, index}) {
                 }
                 have gluten.
             </p>
-            <img src={bread.image} alt={bread.name} />
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
-            
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
               <input type='submit' value="DELETE"/>
             </form>
+            
+            <img src={bread.image} alt={bread.name} />
+
 
             <li><a href="/breads">Go home</a></li>
         </Default>
