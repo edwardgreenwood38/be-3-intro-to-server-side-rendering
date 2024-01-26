2c0c5e9d40cede5accaca0ff244d093cafebbb01
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-01-25 19:37:35 -0500

    method_override added and delete completed

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // DEPENDENCIES
-const express = require('express')
+const express = require('express');
+const methodOverride = require('method-override');
+
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+
 
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 
 // ROUTES
