465bf657606f25cc3a3a9704cb0ec6356727a774
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-02-04 10:52:08 -0500

    updated baker select option to use db

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread.js');
+const Baker = require('../models/baker.js');
 
 
 // INDEX
@@ -19,8 +20,13 @@ breads.get('/', (req, res) => {
 
 // NEW
 breads.get('/new', (req, res) => {
-    res.render('new')
-})
+    Baker.find()
+      .then(foundBakers => {
+        res.render('new', {
+          bakers: foundBakers
+        })
+      });
+});
 
 
 // EDIT
